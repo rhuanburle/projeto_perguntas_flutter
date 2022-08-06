81fec13a303237d739d2103a920b37c8c97f554d
Rhuan Burle <rhuanburlerb@gmail.com>
2022-08-05 21:34:19 -0300

    first commit

--- README.md
+++ README.md
@@ -18,3 +18,4 @@ samples, guidance on mobile development, and a full API reference.
 # projeto_perguntas_flutter
 # projeto_perguntas_flutter
 # projeto_perguntas_flutter
+# projeto_perguntas_flutter
---
+++ lib/quetionario.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'questao.dart';
+import 'resposta.dart';
+
+class Questionario extends StatelessWidget {
+  final List<Map<String, Object>> perguntas;
+  final int perguntaSelecionada;
+  final void Function(int) quantoResponder;
+
+  Questionario(
+      {required this.perguntas,
+      required this.perguntaSelecionada,
+      required this.quantoResponder});
+
+  bool get temPerguntaSelecionada {
+    return perguntaSelecionada < perguntas.length;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Map<String, Object>> respostas = temPerguntaSelecionada
+        ? perguntas[perguntaSelecionada].cast()['respostas']
+        : [];
+
+    return Column(
+      children: [
+        Questao(perguntas[perguntaSelecionada]['texto'].toString()),
+        ...respostas.map((resp) {
+          return Resposta(
+            resp['texto'].toString(),
+            () => quantoResponder(int.parse(resp['pontuacao'].toString())),
+          );
+        }).toList()
+      ],
+    );
+  }
+}
